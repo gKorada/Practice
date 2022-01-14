49b12d30434c86b971d29787774acf762eb8fb0f
gKorada <44787576+gKorada@users.noreply.github.com>
2022-01-14 18:17:19 -0500

    Create SortedLists.java

---
+++ SortedLists.java
@@ -0,0 +1,52 @@
+
+	private Comparator<T> classComparator;
+	
+	public SortedLinkedList(Comparator<T> comparator) {
+		super();
+		classComparator = comparator;
+		
+	}
+	
+	public SortedLinkedList<T> add(T element){
+		Node add = new Node(element);
+		if(head == null) {
+			head = add;
+			tail = add;
+			listSize++;
+			return this;
+		}else {
+			if(classComparator.compare((T)head.data, element) > 0) {
+				super.addToFront(element);
+				listSize++;
+				return this;
+			}else if(classComparator.compare((T)tail.data, element) < 0) {
+				super.addToEnd(element);
+				listSize++;
+				return this;
+			}else {
+				Node temp = head;
+				while(classComparator.compare((T)temp.next.data, element) < 0) {
+					temp = temp.next;
+				}
+				add.next = temp.next;
+				temp = add;
+				listSize++;
+				return this;
+			}
+		}
+	}
+	
+	public SortedLinkedList<T> remove(T targetData){
+		super.remove(targetData, classComparator);
+		return this;
+	}
+	
+	public SortedLinkedList<T> addToFront(T data){
+		throw new UnsupportedOperationException("Invalid Operation for sorted List");
+	}
+	
+	public SortedLinkedList<T> addToEnd(T data){
+		throw new UnsupportedOperationException("Invalid Operation for sorted List");
+	}
+
+}
